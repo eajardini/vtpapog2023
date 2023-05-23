24b74d1e41b9c71d6cf8292ec7ab2f1c9d3df755
Evandro Jardini <eajardini@yahoo.com.br>
2023-05-23 10:48:40 -0300

    Update 20230601_ExercicioSePython02.py

--- 20230601_ExercicioSePython02.py
+++ 20230601_ExercicioSePython02.py
@@ -1,13 +1,15 @@
 '''
 Construa um algoritmo que determine quanto será gasto para encher o
-tanque de um carro. O usuário fornecerá os seguintes dados: Tipo de
-carro (TC) (G – gasolina ou A – álcool) e Capacidade do tanque (CT),
-em litros. Após a escolha do tipo de veículo e da capacidade do tanque,
-o sistema irá imprimir uma mensagem falando o tipo do carro(
-Gasolina ou álcool) e pedirá para o usuário informar o
-valor do preço do litro do combustível.
-Como saída, será informado para o usuário, o valor,
-em reais, do preço de se encher tanque de combustível.
+tanque de um carro. O usuário fornecerá os seguintes dados: 
+ - Tipo de carro variável (TC) que deve ser G – gasolina ou A – álcool.
+ - Capacidade do tanque (variavel CT) em litros. 
+
+Após a escolha do tipo de veículo e da capacidade do tanque,o sistema irá:
+ - Imprimir uma mensagem falando o tipo do carro(Gasolina ou álcool) e pedirá para o usuário informar:
+ - o valor do preço do litro do combustível.
+
+Como saída, será informado para o usuário:
+ - o valor, em reais, do preço de se encher tanque de combustível.
 
 '''
 
